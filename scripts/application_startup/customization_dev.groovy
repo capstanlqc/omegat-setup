e0ef749168edf7904f7ad41573ee92953a41d8db
gergoe <gergoe@users.noreply.github.com>
2023-06-28 16:51:56 +0200

    Update customization_dev.groovy

--- scripts/application_startup/customization_dev.groovy
+++ scripts/application_startup/customization_dev.groovy
@@ -335,9 +335,9 @@ void update_omegat_prefs(localPrefsPath) {
 
     // Try to guess the property type, Boolean, Integer or String
     remotePrefs.each { prop ->
-        if (prop.value == "true" || prop.value == "false") {
+        if (prop instanceof Boolean) {
             Preferences.setPreference(prop.key, prop.value.toBoolean())
-        } else if (prop.value.isInteger()) {
+        } else if (prop instanceof Integer) {
             Preferences.setPreference(prop.key, prop.value.toInteger())
         } else {
             Preferences.setPreference(prop.key, prop.value)
