2a28da95543e70cdd37655b8ec965832694d0b1b
Manuel Souto Pico <m.soutopico@gmail.com>
2023-07-05 17:38:49 +0200

    Update customization_dev.groovy: use abs path to ui_layout.xml in update_ui_layout()
    Hi @briacp I was getting the following error in the `update_ui_layout`: 
    
    ```
    download_asset: https://raw.githubusercontent.com/capstanlqc/omegat-user-config/master/uiLayout.xml
    remote_file_name: uiLayout.xml
    file_location_rel_path_str: uiLayout.xml
    local_path_to_location: /home/souto/.omegat610-dev-custom/uiLayout.xml
    >> DOWNLOAD uiLayout.xml to uiLayout.xml
    %% local_file: /home/souto/.omegat610-dev-custom/uiLayout.xml
    <<<< HANDLING UI LAYOUT >>>>>
    An error occurred
    javax.script.ScriptException: javax.script.ScriptException: java.io.FileNotFoundException: uiLayout.xml (No such file or directory)
    ```
    
    If I understand what this function tries to do, I think that's because it was trying to use the file name (or relative path to the file) and it couldn't find the file. 
    
    I have used the absolute path to the local file as a argument to the function, and it seems to work now. Could you please review that this is the intended logic? Thank you.

--- scripts/customization_dev.groovy
+++ scripts/customization_dev.groovy
@@ -300,7 +300,8 @@ void fetch_files_by_hash(local_file_hash_map, remote_file_hash_map) {
                 if (remote_file_name == "omegat.prefs") {
                     update_omegat_prefs()
                 } else if (remote_file_name == "uiLayout.xml") {
-                    update_ui_layout()
+                    local_uilayout_fpath = config_dir + File.separator + "uiLayout.xml"
+                    update_ui_layout(local_uilayout_fpath)
                 }
                 console.println("????????????????????????????????????")
             }
@@ -319,9 +320,9 @@ void fetch_files_by_hash(local_file_hash_map, remote_file_hash_map) {
 /** Update the UI Layout with the newly download layout. */
 // XXX This effectively prevents users to make adjustments on the UI layout as it will always be
 // reset with the remote layout at each application restart.
-void update_ui_layout() {
+void update_ui_layout(local_uilayout_fpath) {
     console.println("<<<< HANDLING UI LAYOUT >>>>>")
-    new File("uiLayout.xml").withInputStream(is -> {
+    new File(local_uilayout_fpath).withInputStream(is -> {
         Core.getMainWindow().getDesktop().readXML(is)
     })
 }
